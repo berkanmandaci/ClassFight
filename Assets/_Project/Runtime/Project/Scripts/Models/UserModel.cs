61ede1b106e57de6a6ba1796ed88e3b3acb9d345
Berkan <berkan_mandaci@hotmail.com>
2024-12-28 01:39:35 +0300

    user model added

---
+++ Assets/_Project/Runtime/Project/Scripts/Models/UserModel.cs
@@ -0,0 +1,17 @@
+using _Project.Runtime.Core.Extensions.Singleton;
+
+public class UserModel : SingletonBehaviour<UserModel>
+{
+    public long Id { get; set; }
+
+    public string NickName { get; set; }
+
+    public int Level { get; set; }
+
+    public float Experience { get; set; }
+
+    public float ExperienceToNextLevel { get; set; }
+
+    public int Elo { get; set; }
+
+}
