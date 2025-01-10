ce1225ad5d92091f0e254a6c1d7edc69c225a3f0
Berkan <berkan_mandaci@hotmail.com>
2025-01-10 21:13:01 +0300

    tba

--- Assets/_ProjectV2/Runtime/Scripts/Client/ClientPlayerManager.cs
+++ Assets/_ProjectV2/Runtime/Scripts/Client/ClientPlayerManager.cs
@@ -3,10 +3,12 @@ using Fusion;
 using System.Collections.Generic;
 using ProjectV2.Shared;
 using ProjectV2.Core;
+using System;
+using Fusion.Sockets;
 
 namespace ProjectV2.Client
 {
-    public class ClientPlayerManager : NetworkBehaviour
+    public class ClientPlayerManager : NetworkBehaviour, INetworkRunnerCallbacks
     {
         private Dictionary<PlayerRef, NetworkObject> spawnedPlayers = new Dictionary<PlayerRef, NetworkObject>();
         private NetworkObject localPlayerObject;
@@ -73,5 +75,57 @@ namespace ProjectV2.Client
                 Instance = null;
             }
         }
+
+        #region INetworkRunnerCallbacks Implementation
+
+        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+        {
+            Debug.Log($"Player {player} joined");
+        }
+
+        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+        {
+            Debug.Log($"Player {player} left");
+            UnregisterPlayer(player);
+        }
+
+        public void OnInput(NetworkRunner runner, NetworkInput input)
+        {
+            // Input handling will be done in ClientPlayer
+        }
+
+        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+        
+        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+        {
+            Debug.Log($"Shutdown: {shutdownReason}");
+        }
+
+        public void OnConnectedToServer(NetworkRunner runner)
+        {
+            Debug.Log("Connected to server");
+        }
+
+        public void OnDisconnectedFromServer(NetworkRunner runner)
+        {
+            Debug.Log("Disconnected from server");
+        }
+
+        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+        public void OnSceneLoadDone(NetworkRunner runner) { }
+        public void OnSceneLoadStart(NetworkRunner runner) { }
+        public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+        public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+        public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+        public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+
+        #endregion
     }
 } 
